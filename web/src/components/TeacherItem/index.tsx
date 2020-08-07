fd1ca334aa95c535913861ad693b4a1ec5ffdf0d
Neto <nettoveloso@gmail.com>
2020-08-07 01:47:17 -0300

    Finish Web

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -3,37 +3,54 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
+import api from '../../services/api';
 
-function TeacherItem(){
+export interface Teacher {
+    id: number;
+    avatar: string;
+    bio: string;
+    cost: number;
+    name: string;
+    subject: string;        
+    whatsapp: string;
+}
+
+interface TeacherItemPropos {
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemPropos> = ({teacher}) =>{
+
+    function createNewConnection(){
+        api.post('connections', {
+            user_id: teacher.id
+        });
+    }
+
     return (
         <article className="teacher-item">
             <header>
-                <img src="https://avatars0.githubusercontent.com/u/16401171?s=460&u=f6489c8a94db25c88c1f25d8c3747cc20e9b14da&v=4" alt=""/>
+                <img src={teacher.avatar} alt={teacher.name}/>
                 <div>
-                    <strong>Antonio Neto</strong>
-                    <span>Matématica</span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
             </header>
 
-            <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-                when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-                It has survived not only five centuries, but also the leap into electronic typesetting, 
-                remaining essentially unchanged. It was popularised in the 1960s with the release of 
-                Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software 
-                like Aldus PageMaker including versions of Lorem Ipsum.
-            </p>
+            <p>{teacher.bio}</p>
 
             <footer>
                 <p>
                     Preço/hora
-                    <strong>R$ 100,00</strong>
+                    <strong>R$ {teacher.cost}</strong>
                 </p>
-                <button type="button">
+                <a
+                    target="_blank" 
+                    onClick={createNewConnection}
+                    href={`https://wa.me/${teacher.whatsapp}`}>
                     <img src={whatsappIcon} alt="Whatsapp"/>
                     Entrar em contato
-                </button>
+                </a>
             </footer>
         </article>
     )
--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,14 +1,69 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
+import { useHistory } from 'react-router-dom';
 import PageHeader from '../../components/PageHeader';
 import Input from '../../components/Input';
-
-import warningIcon from '../../assets/images/icons/warning.svg';
-
-import './styles.css';
 import Textarea from '../../components/Textarea';
 import Select from '../../components/Select';
 
+import api from '../../services/api';
+import warningIcon from '../../assets/images/icons/warning.svg';
+
+import './styles.css';
+
 function TeacherForm(){
+    const history = useHistory();
+    const [ name, setName ] = useState('');
+    const [ avatar, setAvatar ] = useState('');
+    const [ bio, setBio ] = useState('');
+    const [ whatsapp, setWhatsapp ] = useState('');
+
+    const [ subject, setSubject ] = useState('');
+    const [ cost, setCost ] = useState('');
+
+    const [scheduleItems, setScheduleItems] = useState([
+        { week_day: 0, from: '', to: '' }
+    ]);
+
+    function addNewScheduleItem(){
+        setScheduleItems([
+            ...scheduleItems,
+            { week_day: 0, from: '', to: '' }
+        ]);
+    }
+
+    function setScheduleItemValue(position: number, field: string, value: string){
+        const updateScheduleItems = scheduleItems.map((scheduleItems, index) => {
+            if( index === position ){
+                return { ...scheduleItems, [field]: value }
+            }
+
+            return scheduleItems;
+        })
+
+        setScheduleItems(updateScheduleItems);
+    }
+
+    function handleCreateClass(e: FormEvent){
+        e.preventDefault();
+
+        api.post('classes',{
+            name,
+            avatar,
+            whatsapp,
+            bio,
+            subject,
+            cost: Number(cost),
+            schedule: scheduleItems
+        }).then(()=> {
+            alert('Cadastro Realizado com Sucesso!');
+            history.push('/');
+        }).catch(()=>{
+            alert('Erro no cadastro!');
+        })
+    }
+    
+
+
     return (
         <div id="page-teacher-form" className="container">
             <PageHeader 
@@ -17,33 +72,126 @@ function TeacherForm(){
             />
 
             <main>
-                <fieldset>
-                    <legend>Seus Dados</legend>
-                    <Input name="name" label="Nome Completo" />
-                    <Input name="avatar" label="Avatar" />
-                    <Input name="whatsapp" label="WhatsApp" />
-                </fieldset>
+                <form onSubmit={handleCreateClass}>
+                    <fieldset>
+                        <legend>Seus Dados</legend>
+                        
+                        <Input 
+                            name="name" 
+                            label="Nome Completo" 
+                            value={name} 
+                            onChange={(e)=> {setName(e.target.value)} } 
+                        />
 
-                <fieldset>
-                    <legend>Sobre a aula</legend>
-                    <Select name="subject" label="Matéria" />
-                    <Input name="cost" label="Custo da sua hora por aula" />
-                    <Textarea name="bio" label="Biografia" />
-                </fieldset>
+                        <Input 
+                            name="avatar" 
+                            label="Avatar" 
+                            value={avatar} 
+                            onChange={(e)=> {setAvatar(e.target.value)} } 
+                        />
 
-                <fieldset>
-                    <legend>Horários disponível</legend>
-                    
-                </fieldset>
+                        <Input 
+                            name="whatsapp" 
+                            label="WhatsApp" 
+                            value={whatsapp} 
+                            onChange={(e)=> {setWhatsapp(e.target.value)} } 
+                        />
 
-                <footer>
-                    <p>
-                        <img src={warningIcon} alt="Aviso Importante"/>
-                        Importante! <br />
-                        Preencha todos os dados
-                    </p>
-                    <button type="button">Salvar Cadastro</button>
-                </footer>
+                        <Textarea 
+                            name="bio" 
+                            label="Biografia" 
+                            value={bio} 
+                            onChange={(e)=> {setBio(e.target.value)} } 
+                        />
+                    </fieldset>
+
+                    <fieldset>
+                        <legend>Sobre a aula</legend>
+                        <Select 
+                            name="subject" 
+                            label="Matéria" 
+                            value={subject}
+                            onChange={(e)=> {setSubject(e.target.value)} } 
+                            options={[
+                                { value: 'Artes', label: 'Artes' },
+                                { value: 'Biologia', label: 'Biologia' },
+                                { value: 'Ciências', label: 'Ciências' },
+                                { value: 'Educação Física', label: 'Educação Física' },
+                                { value: 'Geografia', label: 'Geografia' },
+                                { value: 'Português', label: 'Português' },
+                                { value: 'Matemática', label: 'Matemática' },
+                            ]}
+                        />
+
+                        <Input 
+                            name="cost" 
+                            label="Custo da sua hora por aula" 
+                            value={cost}
+                            onChange={(e)=> {setCost(e.target.value)} } 
+                        />
+                        
+                    </fieldset>
+
+                    <fieldset>
+                        <legend>
+                            Horários disponível
+                            <button type="button" onClick={addNewScheduleItem}>
+                                + Novo horário
+                            </button>
+                        </legend>
+
+                        { scheduleItems.map((sheduleItem, index) => {
+                            return (
+                                <div key={sheduleItem.week_day} className="schedule-item">
+                                    <Select 
+                                        name="subject" 
+                                        label="Matéria"
+                                        value={sheduleItem.week_day}
+                                        onChange={ e => setScheduleItemValue(index, 'week_day', e.target.value) } 
+                                        options={[
+                                            { value: '0', label: 'Domigo' },
+                                            { value: '1', label: 'Segunda-feira' },
+                                            { value: '2', label: 'Terça-feira' },
+                                            { value: '3', label: 'Quarta-feira' },
+                                            { value: '4', label: 'Quinta-feira' },
+                                            { value: '5', label: 'Sexta-feira' },
+                                            { value: '6', label: 'Sabado' },
+                                        ]}
+                                    />
+
+                                    <Input 
+                                        name="from" 
+                                        label="Das" 
+                                        type="time" 
+                                        value={sheduleItem.from}
+                                        onChange={ e => setScheduleItemValue(index, 'from', e.target.value) } 
+                                    />
+                                    
+                                    <Input 
+                                        name="to" 
+                                        label="Até" 
+                                        type="time" 
+                                        value={sheduleItem.to}
+                                        onChange={ e => setScheduleItemValue(index, 'to', e.target.value) } 
+                                    />
+                                </div>
+                            );
+                        })}
+                        
+                        
+                    </fieldset>
+
+                    <footer>
+                        <p>
+                            <img src={warningIcon} alt="Aviso Importante"/>
+                            Importante! <br />
+                            Preencha todos os dados
+                        </p>
+                        <button type="submit">
+                            Salvar Cadastro
+                        </button>
+                    </footer>
+                </form>
             </main>
         </div>
     )
